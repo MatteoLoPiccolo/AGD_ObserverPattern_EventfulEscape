eb68bc000a896a404ed9f701ad75ef5e3da63306
MatteoLoPiccolo <lostsoul81x@gmail.com>
2024-06-05 16:47:49 +0200

    Creating LightSwitch Delegate

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -6,12 +6,23 @@ public class LightSwitchView : MonoBehaviour, IInteractable
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
 
+    public delegate void LightSwitchDelegate();
+    public LightSwitchDelegate lightSwitch;
+
+    private void OnEnable()
+    {
+        lightSwitch = OnLightSwitchToggled;
+        lightSwitch += OnLightSwitchSoundEffects;
+    }
+
     private void Start() => currentState = SwitchState.Off;
 
     public void Interact()
     {
         //Todo - Implement Interaction
+        lightSwitch.Invoke();
     }
+
     private void toggleLights()
     {
         bool lights = false;
@@ -33,5 +44,21 @@ public class LightSwitchView : MonoBehaviour, IInteractable
         {
             lightSource.enabled = lights;
         }
+    }
+
+    private void OnLightSwitchToggled()
+    {
+        toggleLights();
+        GameService.Instance.GetInstructionView().HideInstruction();
+        Debug.Log("OnLightSwitchToggled");
+        //GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
+    }
+
+    private void OnLightSwitchSoundEffects()
+    {
+        //toggleLights();
+        //GameService.Instance.GetInstructionView().HideInstruction();
+        Debug.Log("OnLightSwitchSoundEffects");
+        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
     }
 }
