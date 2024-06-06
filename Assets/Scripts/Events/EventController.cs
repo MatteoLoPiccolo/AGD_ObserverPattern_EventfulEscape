6fbef4497bc50afff60c1dba25bef2251a41eaf1
MatteoLoPiccolo <lostsoul81x@gmail.com>
2024-06-06 18:02:58 +0200

    Create an Event Controller

---
+++ Assets/Scripts/Events/EventController.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class EventController
+{
+    public Action baseAction;
+
+    public void AddListener(Action listener) => baseAction += listener;
+
+    public void RemoveListener(Action listener) => baseAction -= listener;
+
+    public void InvokeEvent() => baseAction?.Invoke();
+}
